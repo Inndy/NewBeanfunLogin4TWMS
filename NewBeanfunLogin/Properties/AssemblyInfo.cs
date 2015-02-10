cdf73ef95c0cdcc80fe67575723570495e4c27dd
knowlet <knowlet3389@gmail.com>
2015-02-11 04:07:13 +0800

    update Info

--- NewBeanfunLogin/Properties/AssemblyInfo.cs
+++ NewBeanfunLogin/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
 [assembly: AssemblyTitle("NewBeanfunLogin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MapleStory Launcher")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Inndy")]
+[assembly: AssemblyCompany("KNowlet")]
 [assembly: AssemblyProduct("NewBeanfunLogin")]
-[assembly: AssemblyCopyright("Copyright © Inndy 2013")]
+[assembly: AssemblyCopyright("Copyright © Inndy 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 您可以指定所有的值，也可以依照以下的方式，使用 '*' 將組建和修訂編號
 // 指定為預設值:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
